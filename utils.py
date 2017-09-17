181a0d95ab71e1327775dda94b135c312926adbc
Feiyang Niu <feiyang.niu@gmail.com>
2017-09-17 19:14:41 -0400

    Created the module

---
+++ utils.py
@@ -0,0 +1,11 @@
+import hashlib
+
+
+def md5(fname):
+    """Generating an MD5 checksum of a file
+    """
+    hash_md5 = hashlib.md5()
+    with open(fname, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
