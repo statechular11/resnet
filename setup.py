4ec30cba2c6c173f09722bb8776cda8c52623526
Feiyang Niu <feiyang.niu@gmail.com>
2017-09-17 21:42:36 -0400

    Published on PyPI

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import setup
 setup(name='resnet',
       version='0.1',
       description='keras-style API to ResNets (ResNet-50, ResNet-101, and ResNet-152)',
+      keywords='ResNets ResNet-101 ResNet-152 keras',
       url='https://github.com/statech/resnet',
       author='Feiyang Niu',
       author_email='feiyang.niu@gmail.com',
       license='MIT',
       packages=['resnet'],
+      install_requires=[
+          'hashlib',
+          'keras>=2.0'
+      ],
+      include_package_data=True,
       zip_safe=False)
